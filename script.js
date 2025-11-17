4535027cbda030a597a36cdbfc0ed771e5a12c18
sebasdev <sebassandovaldev@gmail.com>
2025-11-16 20:37:58 -0600

    feat: seccion FAQ con acordeon simple en JS

--- script.js
+++ script.js
@@ -17,3 +17,20 @@ if (form && state) {
     form.reset();
   });
 }
+// FAQ: abrir/cerrar respuestas
+const faqItems = document.querySelectorAll(".faq__item");
+
+faqItems.forEach((item) => {
+  const button = item.querySelector(".faq__question");
+  const answer = item.querySelector(".faq__answer");
+
+  button.addEventListener("click", () => {
+    const isOpen = item.classList.contains("is-open");
+    // cerrar todos
+    faqItems.forEach((other) => other.classList.remove("is-open"));
+    // si este no estaba abierto, abrirlo
+    if (!isOpen) {
+      item.classList.add("is-open");
+    }
+  });
+});
